5c13fbf47303127391945263f6d1de0fe2054680
krupal <bhuvakrupal01@gmail.com>
2022-07-27 11:43:02 +0530

    ADD : Google Map

--- app/src/main/java/com/example/mapwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/mapwithcompose/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.mapwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mapwithcompose.ui.theme.MapWithComposeTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,26 +25,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             MapWithComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainMapView()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainMapView() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MapWithComposeTheme {
-        Greeting("Android")
+    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+    val properties by remember { mutableStateOf(MapProperties(mapType = MapType.SATELLITE)) }
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+
+
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        GoogleMap(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.7f),
+            properties = properties,
+            uiSettings = uiSettings,
+            cameraPositionState = cameraPositionState
+        ){
+            Marker(
+                state = MarkerState(position = singapore),
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
+        Switch(
+            checked = uiSettings.zoomControlsEnabled,
+            onCheckedChange = {
+                uiSettings = uiSettings.copy(zoomControlsEnabled = it)
+            }
+        )
     }
 }
